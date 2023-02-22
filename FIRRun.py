c88de706ddaab5901b20a5853ad6fec14519ea7f
Yuan Jin <jin_yuan0928@163.com>
2023-02-22 09:59:03 +0100

    modify software architecture

--- FIRRun.py
+++ FIRRun.py
@@ -1,20 +1,29 @@
-import numpy as np
-from shared import *
 
-#-----implementation of Hard-wired weighted average (HWA) design-----#
-class HWAFIR():
+def cvtRun(self, RNS):
+        RNS = np.array(RNS)
+        tap = len(self.input)
+        selNum = np.log2(tap)
+        inputSC = np.empty(RNS.shape[0], tap, dtype=bool)
+        weightSC = np.empty(RNS.shape[0], tap, dtype=bool)
+        selSC = np.empty(RNS.shape[0], selNum, dtype=bool)
 
-    def __init__(self):
-        self.input = None
-        self.weight = None
+        for i in range(tap):
+            inputSC[:, i] = RNS[:, 0] < self.input[i]
+            weightSC[:, i] = RNS[:, 1] < self.weight[i]
+        for i in range(selNum):
+            selSC[:, i] = RNS[:, 2+i] < 0.5
+        
+        productSC = np.logical_not(np.logical_xor(inputSC, weightSC))
+        outputSC = np.empty(RNS.shape[0], dtype=bool)
+        for i in range(RNS.shape[0]):
+            outputSC[i] = productSC[i, bin2Float(selSC[i, :])]
+        
+        n1 = (outputSC == True).sum()
+        n0 = (outputSC == False).sum()
+        result = (n1-n0)/(n1+n0)
+        return result
 
-    def getInput(self, input):
-        self.input = np.array(input)
-
-    def getWeight(self, weight):
-        self.weight = np.array(weight)
-
-    def run(self, RNS, m):
+def HWARun(self, RNS, m):
         RNS = np.array(RNS)
         tap = len(self.input)
         inputSC = np.empty(RNS.shape[0], tap, dtype=bool)
@@ -46,4 +55,3 @@ class HWAFIR():
         n0 = (outputSC == False).sum()
         result = (n1-n0)/(n1+n0)
         return result   
-        
--- weightGen.py
+++ weightGen.py
@@ -5,6 +5,7 @@ class weightGen():
     
     #-----generate weight-----#
     def gen(self, num):
+        
         rng = np.random.default_rng()
         rn = rng.uniform(-1,1,num)
 
