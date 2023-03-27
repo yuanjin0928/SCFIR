9ac3e58e9a2472f357dce8e0f96f709a337847e2
Yuan Jin <jin_yuan0928@163.com>
2023-03-28 01:22:06 +0200

    add Input_Lfsr_Sel_Halton

---
+++ Input_Lfsr_Sel_Halton_tb.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import inputGen
+import weightGen
+import test
+
+"""generate input"""
+power = 5
+num, times = 2**power, 100
+samples = inputGen.uniform(num, times)
+"""generate weight"""
+weight = weightGen.lowPassFir(0.2, 31)
+
+"""test effect of sc's length on the precision of filter"""
+minLen = 8
+maxLen = 20
+
+"""run filter"""
+CWAError = test.Test_Input_Lfsr_Sel_Halton('CWA', minLen, maxLen, samples, weight)
+HWAError = test.Test_Input_Lfsr_Sel_Halton('HWA', minLen, maxLen, samples, weight)
+MWAError = test.Test_Input_Lfsr_Sel_Halton('MWA', minLen, maxLen, samples, weight)
+OLMUXError = test.Test_Input_Lfsr_Sel_Halton('OLMUX', minLen, maxLen, samples, weight)
+
+# CWAError = list(range(minLen, maxLen+1))
+# HWAError = list(range(minLen, maxLen+1))
+# MWAError = list(range(minLen, maxLen+1))
+# OLMUXError = list(range(minLen, maxLen+1))
+
+"""plot result"""
+xaxis = list(range(minLen, maxLen+1))
+
+plt.plot(xaxis, CWAError, 'b-o', linewidth = 3, label='CWA')
+plt.plot(xaxis, HWAError, 'g-o', linewidth = 3, label='HWA')
+plt.plot(xaxis, MWAError, 'r-o', linewidth = 3, label='MWA')
+plt.plot(xaxis, OLMUXError, 'k-o', linewidth=3, label='OLMUX')
+for i in range(len(xaxis)):
+    plt.text(xaxis[i], CWAError[i], str(round(CWAError[i], 5)))
+    plt.text(xaxis[i], HWAError[i], str(round(HWAError[i], 5)))
+    plt.text(xaxis[i], MWAError[i], str(round(MWAError[i], 5))) 
+    plt.text(xaxis[i], OLMUXError[i], str(round(OLMUXError[i], 5)))
+plt.title('RMSE of different SC-based FIR filter with Lfsr as input and Halton as Selection', fontsize=20)
+plt.legend(fontsize=14)
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
--- test.py
+++ test.py
@@ -130,8 +130,49 @@ def Test_Input_Halton_Sel_Lfsr(arch, minLen, maxLen, samples, weight):
         for i in range(samplesPower):
             rns[:, Filter[arch]['rns']+i] = np.load(os.path.join(rngLfsrFolder,rngLfsrFiles[i]))
 
-        """selece halton rns"""
+        """select halton rns"""
         rns[:, :Filter[arch]['rns']] = rnsHaltonSource[:2**rnsLen, :]
+        
+        """filter run"""
+        result, calib = Filter[arch]['func'](samples, weight, rns) 
+        error[rnsLen-minLen] = (np.sum(np.power(Ref-calib, 2))/numOfExprm)**(1/2)
+    
+    return error
+
+def Test_Input_Lfsr_Sel_Halton(arch, minLen, maxLen, samples, weight):
+    CWD = os.getcwd()
+    samplesPower = int(math.log2(len(weight)))
+    numOfExprm = samples.shape[1]
+
+    """calculate reference"""
+    Ref = np.dot(np.transpose(samples), weight)
+    error = np.empty(maxLen-minLen+1)
+    
+    """load halton rns"""
+    rngHaltonDir = os.path.join(CWD, 'rng/halton') 
+    rngHaltonFolders = os.listdir(rngHaltonDir)
+    rngHaltonFolders = sorted([int(element) for element in rngHaltonFolders])
+    index = bisect.bisect_left(rngHaltonFolders, maxLen)
+    rngHaltonSubDir = os.path.join(rngHaltonDir, '{}'.format(rngHaltonFolders[index]))
+    rnsHaltonSource = np.empty((2**rngHaltonFolders[index], samplesPower))
+    rngHaltonFiles = os.listdir(rngHaltonSubDir)
+    rngHaltonFiles = sorted([int(element.rstrip('.npy')) for element in rngHaltonFiles])
+    for i in range(samplesPower):
+        fileName = os.path.join(rngHaltonSubDir, '{}.npy'.format(rngHaltonFiles[i]))
+        rnsHaltonSource[:, i] = np.load(fileName)
+    
+    for rnsLen in range(minLen, maxLen+1):
+        """select rns"""
+        rns = np.empty((2**rnsLen, samplesPower+Filter[arch]['rns']))
+
+        """load lfsr rns"""
+        rngLfsrFolder = os.path.join(CWD, 'rng/lfsr', '{}'.format(rnsLen))
+        rngLfsrFiles = sorted(os.listdir(rngLfsrFolder))
+        for i in range(Filter[arch]['rns']):
+            rns[:, i] = np.load(os.path.join(rngLfsrFolder,rngLfsrFiles[i]))
+
+        """select halton rns"""
+        rns[:, Filter[arch]['rns']:] = rnsHaltonSource[:2**rnsLen, :]
         
         """filter run"""
         result, calib = Filter[arch]['func'](samples, weight, rns) 
